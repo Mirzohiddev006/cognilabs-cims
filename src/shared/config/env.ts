75d36e84c94b72e0b3c114544caaf999b5974256
Mirzohiddev006 <ibrohimjonovm2006@gmail.com>
2026-03-28 22:32:35 +0500

    fixed-14

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,9 +1,13 @@
 const fallbackApiBaseUrl = 'https://api.project.cims.cognilabs.org/'
-const fallbackWebsiteStatsApiBaseUrl = 'https://height-varied-civilian-plastic.trycloudflare.com/'
+const fallbackWebsiteStatsApiBaseUrl = fallbackApiBaseUrl
+
 
 export const env = {
   appName: import.meta.env.VITE_APP_NAME || 'Cognilabs CIMS',
   apiBaseUrl: import.meta.env.VITE_API_BASE_URL || fallbackApiBaseUrl,
-  websiteStatsApiBaseUrl: import.meta.env.VITE_WEBSITE_STATS_API_BASE_URL || fallbackWebsiteStatsApiBaseUrl,
+  websiteStatsApiBaseUrl:
+    import.meta.env.VITE_WEBSITE_STATS_API_BASE_URL ||
+    import.meta.env.VITE_API_BASE_URL ||
+    fallbackWebsiteStatsApiBaseUrl,
   appEnv: import.meta.env.VITE_APP_ENV || 'development',
 } as const
